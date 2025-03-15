55d198e5f2a519fc91e37e3510d07780fb8a61e8
Klay <arqkdias@gmail.com>
2025-03-14 20:51:30 -0400

    feat: clicando no menu vai para a seção selecionada

--- src/scripts/main.js
+++
@@ -1,7 +0,0 @@
-import { fetchData } from "./modules/api.js";
-import { setupNavbar } from "./components/navbar.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-    setupNavbar();
-    fetchData();
-});
